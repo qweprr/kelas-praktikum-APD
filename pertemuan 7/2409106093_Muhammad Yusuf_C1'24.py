ad826329097747908055a6e98eed2039449f79be
qweprr <itsuno06@gmail.com>
2024-10-16 16:34:07 +0800

    pertemuan 7

---
+++ pertemuan 7/2409106093_Muhammad Yusuf_C1'24.py
@@ -0,0 +1,114 @@
+# # def salam():
+# #     print("halo selamat sore")
+    
+# # salam()
+    
+# # # pilihan = 0
+
+
+# # # if pilihan == 1:
+# # #     salam()
+# # # elif pilihan == 2:
+# # #     salam()
+# # # elif pilihan == 3:
+# # #     salam()
+# # # elif pilihan == 4:
+# # #     salam()
+# # # elif pilihan == 5:
+# # #     salam()
+
+# def penjumlahan():
+#     hasil = 7+3
+#     print(hasil)
+# penjumlahan()
+
+
+
+# def penjumlah(a,b,c):
+#     hasil = a+b+c
+#     print(hasil)
+    
+# penjumlah(3,4,4)
+
+
+
+
+# a = int(input ("Bilangan pertama: "))
+# b = int(input ("Bilangan kedua: "))
+# c = int(input ("Bilangan ketiga: "))
+
+# def penjumlah(a,b,c):
+#     hasil = a+b+c
+#     print(hasil)
+    
+# penjumlah(a,b,c)
+
+
+# def perkalian(a,b):
+#     hasil = a*b 
+#     print(hasil)
+    
+# perkalian(3,5)
+
+# def luas_persegi(sisi):
+#     luas = sisi*sisi
+#     print(f"Luas perseigi : {luas}")
+#     print(luas)
+    
+# sisi = int(input("sisi persegi : "))
+
+# luas_persegi(sisi)
+
+# def Volume_persegi(sisi):
+#     luas = sisi*sisi*sisi
+#     print(f"Luas Persegi : ")
+#     print(luas)
+    
+# sisi = int(input("sisi persegi : "))
+
+# Volume_persegi(sisi)
+
+
+# def kali(a,b):
+#     hasil = a*b
+#     return hasil
+
+# print(kali(5,5))
+
+
+# # rumus: sisi x sisi
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     return luas
+# # rumus: sisi x sisi x sisi
+# def volume_persegi(sisi):
+#     volume = luas_persegi(sisi) * sisi
+#     print ("Volume Persegi = ", volume)
+# # pemanggilan Fungsi
+
+# volume_persegi(6)
+
+
+
+
+# a = int(input("masukan angka : "))
+
+# def penjumlahan(a):
+#     hasil = a+a
+#     return hasil
+
+# def perkalian(a):
+#     hasil = penjumlahan(a)*a
+#     print(f"hasil Perkallian: {hasil}")
+# perkalian(a)
+
+a = 1
+b = 2
+
+def penjumlahan(a,b):
+    a=5
+    b=4
+    hasil = a+b
+    print (hasil)
+    
+penjumlahan(a,b)
